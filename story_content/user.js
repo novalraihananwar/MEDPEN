8ea5c3ccb6412a6374e55de97a54c124e56cf5a2
novalraihananwar <137134656+novalraihananwar@users.noreply.github.com>
2025-12-10 21:00:53 +0700

    File Upload

---
+++ story_content/user.js
@@ -0,0 +1,29 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6E5XMftwHf9":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+
+
+audio.src="1.mp3";
+
+
+audio.load();
+
+
+audio.play();
+
+
+audio.volume = 
+
+0.4;
+}
+
